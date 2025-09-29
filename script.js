78f3981ff5fe941d4c56b631203373fee151fd25
qillzzzzzzz <qilasolo20@gmail.com>
2025-09-29 22:26:00 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,603 @@
+// Data Produk
+const produk = [
+
+  //gelang
+  { 
+    nama: "Gelang Cristal Meow", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/gelang_manik/gelang1-putih.jpg", 
+    varian: [
+      "images/gelang_manik/gelang1-putih.jpg", 
+      "images/gelang_manik/gelang1-ungu.jpg",
+      "images/gelang_manik/gelang1-pink.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "produk-baru" 
+  },
+  { 
+    nama: "Gelang Kupu Bunga", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/gelang_manik/gelang2-pink.jpg", 
+    varian: [
+      "images/gelang_manik/gelang2-pink.jpg",
+      "images/gelang_manik/gelang2-ungu.jpg",
+      "images/gelang_manik/gelang2-biru.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Kupu Cristal Bunga", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/gelang_manik/gelang3-pink.jpg", 
+    varian: [
+      "images/gelang_manik/gelang3-pink.jpg",
+      "images/gelang_manik/gelang3-ungu.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Bintang Cristal", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/gelang_manik/gelang4.jpg", 
+    varian: [
+      "images/gelang_manik/gelang4.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Clover Cristal", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/gelang_manik/gelang5.jpg", 
+    varian: [
+      "images/gelang_manik/gelang5.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Manik Cristal", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/gelang_manik/gelang6-pink.jpg", 
+    varian: [
+      "images/gelang_manik/gelang6-pink.jpg",
+      "images/gelang_manik/gelang6-ungu.jpg",
+      "images/gelang_manik/gelang6-ungu.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "" 
+  },
+  { 
+    nama: "Gelang Surga Kupu-Kupu", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/gelang_manik/gelang7-pink.jpg", 
+    varian: [
+      "images/gelang_manik/gelang7-pink.jpg",
+      "images/gelang_manik/gelang7-ungu.jpg",
+      "images/gelang_manik/gelang7-ungu.jpg",
+    ],
+    kategori: "gelang", 
+    tag: ""  
+  },
+  { 
+    nama: "Gelang Manik Cristal Simple", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/gelang_manik/gelang8-putih.jpg", 
+    varian: [
+      "images/gelang_manik/gelang8-putih.jpg",
+      "images/gelang_manik/gelang8-biru.jpg",
+    ],
+    kategori: "gelang", 
+    tag: ""  
+  },
+  { 
+    nama: "Gelang Bulan Sabit Cristal", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/gelang_manik/gelang9.jpg", 
+    varian: [
+      "images/gelang_manik/gelang9.jpg",
+    ],
+    kategori: "gelang", 
+    tag: ""  
+  },
+  { 
+    nama: "Gelang Kupu-Kupu", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/gelang_manik/gelang10-pink.jpg", 
+    varian: [
+      "images/gelang_manik/gelang10-pink.jpg",
+      "images/gelang_manik/gelang10-ungu.jpg",
+      "images/gelang_manik/gelang10-ungu.jpg",
+    ],
+    kategori: "gelang", 
+    tag: ""  
+  },
+  { 
+    nama: "Gelang Cristal Ceko Pink", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/gelang_manik/gelang11-sedang.jpg", 
+    varian: [
+      "images/gelang_manik/gelang11-besar.jpg",
+      "images/gelang_manik/gelang11-sedang.jpg",
+      "images/gelang_manik/gelang11-kecil.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+
+  //gelang tali
+  { 
+    nama: "Gelang Couple Spiderman ", 
+    harga: "Rp 8.000", 
+    fotoUtama: "images/gelang_tali/gelangtali1.jpg", 
+    varian: [
+      "images/gelang_tali/gelangtali1.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Couple Spiderman ", 
+    harga: "Rp 4.000", 
+    fotoUtama: "images/gelang_tali/gelangtali1.jpg", 
+    varian: [
+      "images/gelang_tali/gelangtali1_putih.jpg",
+      "images/gelang_tali/gelangtali1_hitam.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Couple Dino ", 
+    harga: "Rp 8.000", 
+    fotoUtama: "images/gelang_tali/gelangtali2.jpg", 
+    varian: [
+      "images/gelang_tali/gelangtali2.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Polos", 
+    harga: "Rp 3.000", 
+    fotoUtama: "images/gelang_tali/gelangtali3.jpg", 
+    varian: [
+      "images/gelang_tali/gelangtali3.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Tali", 
+    harga: "Rp 4.000", 
+    fotoUtama: "images/gelang_tali/gelangtali5.jpg", 
+    varian: [
+      "images/gelang_tali/gelangtali5.jpg",
+      "images/gelang_tali/gelangtali4.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Couple Heart", 
+    harga: "Rp 8.000", 
+    fotoUtama: "images/gelang_tali/gelangtali6.jpg", 
+    varian: [
+      "images/gelang_tali/gelangtali3.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Tali Inisial A-Z", 
+    harga: "Rp 4.000", 
+    fotoUtama: "images/gelang_tali/gelangtali7.jpg", 
+    varian: [
+      "images/gelang_tali/gelangtali7.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Tali Tridatu Polos", 
+    harga: "Rp 3.000", 
+    fotoUtama: "images/gelang_tali/gelang_ali9.jpg", 
+    varian: [
+      "images/gelang_tali/gelangtali9.jpg",
+      "images/gelang_tali/gelangtali12.jpg",
+      "images/gelang_tali/gelangtali11.jpg",
+      "images/gelang_tali/gelangtali10.jpg",
+      "images/gelang_tali/gelangtali8.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Tali Tridatu Batu Cina", 
+    harga: "Rp 8.000", 
+    fotoUtama: "images/gelang_tali/gelangtali9.jpg", 
+    varian: [
+      "images/gelang_tali/gelangtali13_hijau.jpg",
+      "images/gelang_tali/gelangtali13_oren.jpg",
+      "images/gelang_tali/gelangtali13_merah.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Tali Tridatu 3 Batu Cina", 
+    harga: "Rp 15.000", 
+    fotoUtama: "images/gelang_tali/gelangtali14.jpg", 
+    varian: [
+      "images/gelang_tali/gelangtali14.jpg",
+    ],
+    kategori: "gelang", 
+    tag: "best-seller"  
+  },
+  //gelang anak
+  { 
+    nama: "Gelang Anak Pinky Tali", 
+    harga: "Rp 8.000", 
+    fotoUtama: "images/gelang_anak/gelanganak1.jpg", 
+    varian: [
+      "images/gelang_anak/gelanganak1.jpg",
+    ],
+    kategori: "gelang anak", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Bayi Bunga", 
+    harga: "Rp 5.000", 
+    fotoUtama: "images/gelang_anak/gelanganak2.jpg", 
+    varian: [
+      "images/gelang_anak/gelanganak2.jpg",
+    ],
+    kategori: "gelang anak", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Bayi", 
+    harga: "Rp 5.000", 
+    fotoUtama: "images/gelang_anak/gelanganak3.jpg", 
+    varian: [
+      "images/gelang_anak/gelanganak3.jpg",
+    ],
+    kategori: "gelang anak", 
+    tag: "produk-baru"  
+  },
+  { 
+    nama: "Gelang Bayi Bunga Tali", 
+    harga: "Rp 7.000", 
+    fotoUtama: "images/gelang_anak/gelanganak4.jpg", 
+    varian: [
+      "images/gelang_anak/gelanganak4.jpg",
+    ],
+    kategori: "gelang anak", 
+    tag: "best-seller"  
+  },
+  { 
+    nama: "Gelang Anak Pita", 
+    harga: "Rp 7.000", 
+    fotoUtama: "images/gelang_anak/gelanganak5.jpg", 
+    varian: [
+      "images/gelang_anak/gelanganak5.jpg",
+    ],
+    kategori: "gelang anak", 
+    tag: "produk-baru"  
+  },
+
+  //gantungan kunci
+  { 
+    nama: "Cats Bag Charms", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/products/gantungankunci1_oren.jpg", 
+    varian: [
+      "images/gantungan_kunci/gantungankunci1_oren.jpg",
+      "images/gantungan_kunci/gantungankunci1_kuning.jpg",
+      "images/gantungan_kunci/gantungankunci1_biru.jpg",
+      "images/gantungan_kunci/gantungankunci1_putih.jpg",
+    ], 
+    kategori: "gantungan", 
+    tag: " " 
+  },
+  { 
+    nama: "Black n White Love Bag Charm", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/products/gantungankunci2.jpg", 
+    varian: ["images/gantungan_kunci/gantungankunci2.jpg",
+
+    ],
+    kategori: "gantungan", 
+    tag: " " 
+  },
+  { 
+    nama: "Gantungan Lucu", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/products/gantungankunci3_pink.jpg", 
+    varian: [
+      "images/gantungan_kunci/gantungankunci3_pink.jpg",
+      "images/gantungan_kunci/gantungankunci3_putih.jpg",
+      "images/gantungan_kunci/gantungankunci3_biru.jpg",
+    ], 
+    kategori: "gantungan", 
+    tag: "best-seller" 
+  },
+  { 
+    nama: "Gantungan Kamboja", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/products/gantungankamboja_pink.jpg", 
+    varian: [
+      "images/gantungan_kunci/gantungankamboja_coklat.jpg",
+      "images/gantungan_kunci/gantungankamboja_grey.jpg",
+      "images/gantungan_kunci/gantungankamboja_kuning.jpg",
+      "images/gantungan_kunci/gantungankamboja_pink.jpg",
+      "images/gantungan_kunci/gantungankamboja_ungu.jpg",
+    ], 
+    kategori: "gantungan", 
+    tag: "best-seller" 
+  },
+  { 
+    nama: "Gantungan Flowers", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/products/gantungankunci5_pinkfanta.jpg", 
+    varian: [
+      "images/gantungan_kunci/gantungankunci5_pinkfanta.jpg",
+      "images/gantungan_kunci/gantungankunci5_softpink.jpg",
+      "images/gantungan_kunci/gantungankunci5_ungu.jpg",
+      "images/gantungan_kunci/gantungankunci5_kuning.jpg",
+      "images/gantungan_kunci/gantungankunci5_biru.jpg",
+    ], 
+    kategori: "gantungan", 
+    tag: "best-seller" 
+  },
+  { 
+    nama: "Gantungan Love Pom", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/products/gantungankunci6_pink.jpg", 
+    varian: [
+      "images/gantungan_kunci/gantungankunci6_pink.jpg",
+      "images/gantungan_kunci/gantungankunci6_hitam.jpg",
+      "images/gantungan_kunci/gantungankunci6_ungu.jpg",
+    ], 
+    kategori: "gantungan", 
+    tag: "best-seller" 
+  },
+  { 
+    nama: "Gantungan Inisal A-Z", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/products/gantungankunci6_inisal.jpg", 
+    varian: [
+      "images/gantungan_kunci/gantungankunci6_inisal.jpg",
+    ], 
+    kategori: "gantungan", 
+    tag: "best-seller" 
+  },
+  { 
+    nama: "Gantungan Love Pom", 
+    harga: "Rp 10.000", 
+    fotoUtama: "images/products/gantungankunci6_pink.jpg", 
+    varian: [
+      "images/gantungan_kunci/gantungankunci6_pink.jpg",
+      "images/gantungan_kunci/gantungankunci6_hitam.jpg",
+      "images/gantungan_kunci/gantungankunci6_ungu.jpg",
+    ], 
+    kategori: "gantungan", 
+    tag: "best-seller" 
+  },
+
+  //gantungan macrame
+{ 
+    nama: "Gantungan Tas Macrame Anggrek", 
+    harga: "Rp 18.000", 
+    fotoUtama: "images/gantungan_macrame/gantunganmacrame1_pink.jpg", 
+    varian: [
+      "images/gantungan_macrame/gantungamacrame1_pink.jpg",
+      "images/gantungan_macrame/gantungamacrame1_biru.jpg",
+      "images/gantungan_macrame/gantungamacrame1_ungu.jpg",
+      "images/gantungan_macrame/gantungamacrame1_ungu2.jpg",
+      "images/gantungan_macrame/gantungamacrame1_biru2.jpg",
+    ], 
+    kategori: "macrame",
+    tag: "best-seller" 
+  },
+{ 
+    nama: "Gantungan Tas Macrame ", 
+    harga: "Rp 5.000", 
+    fotoUtama: "images/gantungan_macrame/pelangi_macrame1.jpg", 
+    varian: [
+      "images/gantungan_macrame/pelangi_macrame1.jpg",
+      "images/gantungan_macrame/pelangi_macrame2.jpg",
+      "images/gantungan_macrame/pelangi_macrame3.jpg",
+    ], 
+    kategori: "macrame",
+    tag: "best-seller" 
+  },
+{ 
+    nama: "Gantungan Tas Macrame", 
+    harga: "Rp 18.000", 
+    fotoUtama: "images/gantungan_macrame/rumbai_macrame1.jpg", 
+    varian: [
+      "images/gantungan_macrame/rumbai_macrame1.jpg",
+      "images/gantungan_macrame/rumbai_macrame2.jpg",
+      "images/gantungan_macrame/rumbai_macrame3.jpg",
+
+    ], 
+    kategori: "macrame",
+    tag: "best-seller" 
+  },
+];
+
+
+function gantiFoto(el) {
+  const mainImage = el.closest('.produk-card').querySelector('.main-image');
+  mainImage.src = el.src;
+
+  // kasih highlight di varian yang dipilih
+  el.closest('.color-options').querySelectorAll('img').forEach(img => {
+    img.classList.remove('active');
+  });
+  el.classList.add('active');
+}
+ //supaya semua tag muncul
+ function createTagLabels(tags) {
+  return tags.map(t => `<span class="tag ${t}">${t}</span>`).join(" ");
+}
+
+function createCards(data) {
+  return data.map(p => `
+    <div class="produk-card">
+      <img src="${p.fotoUtama}" alt="${p.nama}" class="main-image">
+      <h3>${p.nama}</h3>
+      <p>${p.harga}</p>
+      <div class="tags">
+        ${createTagLabels(p.tag)}
+      </div>
+      <button onclick="beliProdukLangsung(this)">Beli Sekarang</button>
+
+    </div>
+  `).join("");
+}
+
+
+
+
+// Fungsi Buat Kartu Produk
+function createCards(arr) {
+  return arr.map(item => `
+    <div class="produk-card" data-aos="fade-up">
+      <img class="main-image" 
+           src="${item.varian && item.varian.length > 0 ? item.varian[0] : item.foto}" 
+           alt="${item.nama}">
+      
+      ${item.varian ? `
+        <div class="color-options">
+          ${item.varian.map((v, i) => `
+            <img src="${v}" onclick="gantiFoto(this)" ${i === 0 ? 'class="active"' : ''}>
+          `).join('')}
+        </div>
+      ` : ''}
+
+      <h3>${item.nama}</h3>
+      <p class="harga">${item.harga}</p>
+      <button onclick="beliProdukLangsung(this)">Beli Sekarang</button>
+
+    </div>
+  `).join('');
+}
+
+
+
+
+// Render Best Seller
+function renderBestSeller() {
+  const bestSeller = produk.filter(p => p.tag === "best-seller");
+  document.getElementById('best-seller').innerHTML = createCards(bestSeller);
+}
+
+// Render Produk Baru
+function renderProdukBaru() {
+  const produkBaru = produk.filter(p => p.tag === "produk-baru");
+  document.getElementById('produk-baru').innerHTML = createCards(produkBaru);
+}
+
+// Render Semua / Filter Kategori
+function renderProduk(kategori = "all") {
+  const filtered = kategori === "all" ? produk : produk.filter(p => p.kategori === kategori);
+  const container = document.getElementById('produk-container');
+  container.classList.add('fade-out');
+  setTimeout(() => {
+    container.innerHTML = createCards(filtered);
+    container.classList.remove('fade-out');
+    AOS.refresh(); // refresh AOS after DOM change
+  }, 300);
+}
+
+// setelah renderBestSeller dan renderProdukBaru
+function setupSliderControls(wrapperSelector) {
+  const wrapper = document.querySelector(wrapperSelector);
+  if (!wrapper) return;
+
+  const container = wrapper.querySelector('.slider-container');
+  const btnPrev = wrapper.querySelector('.slider-btn.prev');
+  const btnNext = wrapper.querySelector('.slider-btn.next');
+
+  const scrollAmount = 300; // bisa kamu ubah sesuai lebar card
+
+  btnPrev.addEventListener('click', () => {
+    container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+  });
+  btnNext.addEventListener('click', () => {
+    container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+  });
+}
+
+// Panggil setelah produk dirender
+renderBestSeller();
+renderProdukBaru();
+renderProduk(); // katakan produknya
+
+// Setup tombol slider
+setupSliderControls('section#katalog .slider-wrapper:nth-of-type(1)'); // untuk best seller
+setupSliderControls('section#katalog .slider-wrapper:nth-of-type(2)'); // untuk produk baru
+
+
+// Hamburger menu toggle
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  hamburger.textContent = navLinks.classList.contains('active') ? "✖" : "☰";
+});
+
+document.querySelectorAll('#nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('active');
+    hamburger.textContent = "☰";
+  });
+});
+
+
+
+// Event Listener Kategori
+document.querySelectorAll('#kategori-buttons button').forEach(btn => {
+  btn.addEventListener('click', () => {
+    renderProduk(btn.getAttribute('data-kategori'));
+  });
+});
+
+// Smooth scroll untuk navbar
+document.querySelectorAll('.navbar nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Load awal
+renderBestSeller();
+renderProdukBaru();
+renderProduk();
+
+
+//section beli sekarang
+function beliProdukLangsung(el) {
+  const card = el.closest('.produk-card');
+  const nama = card.querySelector('h3').innerText;
+  const harga = card.querySelector('.harga').innerText;
+  const varianAktif = card.querySelector('.color-options img.active');
+  const varian = varianAktif ? varianAktif.src.split('/').pop() : ''; // ambil nama file varian
+
+  const pesan = encodeURIComponent(
+    `Halo kak, saya tertarik dengan:\n\n📌 ${nama}${varian ? ' (' + varian + ')' : ''}\n💰 Harga: ${harga}\n\nApakah stok ini masih tersedia?`
+  );
+
+  const nomorWA = "6289697710601"; // ganti sesuai admin
+  const waUrl = `https://wa.me/${nomorWA}?text=${pesan}`;
+  window.open(waUrl, '_blank');
+}
+
